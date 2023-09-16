f6e555baf0d1c10421083c909bf2572da3c7247b
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2023-09-16 17:33:55 -0300

    feat: created abstract `LandVechile` class

---
+++ src/com/vehicle/LandVehicle.java
@@ -0,0 +1,22 @@
+package com.vehicle;
+
+public abstract class LandVehicle {
+  protected String model;
+  protected String plate;
+  protected String driver;
+
+  public LandVehicle(String model, String plate) {
+    this.model = model;
+    this.plate = plate;
+  }
+
+  public void setDriver(String name) {
+    this.driver = name;
+  }
+
+  public String getPlate() {
+    return this.plate;
+  }
+
+  public abstract String detail();
+}
